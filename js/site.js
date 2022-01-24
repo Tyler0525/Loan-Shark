2e23ae2c56a182e80f8c11c90b9ce496711a0080
Tyler0525 <96202897+Tyler0525@users.noreply.github.com>
2022-01-24 14:29:42 -0800

    intial commit

--- js/site.js
+++ js/site.js
@@ -1,13 +1,137 @@
 // display amesage to the user
-function getMessage() {
 
-    let userMessage = document.getElementById("message").value;
-    
-Swal.fire({
-    icon: 'success',
-    title: 'THE APP SAYS',
-    userMessage,
-})
+//Caluculate the payment for the loan
+function calcPayment(amount, rate, term) {
+    return (amount * rate) / (1 - Math.pow(1 + rate, -term));
+}
 
-    console.log("Hello Console Log");
+
+//caluctlate the interest for the current balance of the loan 
+function calcInterest(balance, rate) {
+    return balance * rate;
+}
+
+
+//convert rate to a monthly interest rate 
+function calcRate(rate) {
+    return rate = rate / 1200;
+}
+
+function getValues() {
+    let loanAmount = document.getElementById("lamount").value;
+    //months inputed from the user 
+    let loanTerm = document.getElementById("lterm").value;
+    let loanRate = document.getElementById("lrate").value;
+
+    loanAmount = Number(loanAmount);
+    loanTerm = parseInt(loanTerm);
+
+
+    loanRate = parseFloat(loanRate);
+    loanRate = calcRate(loanRate);
+
+    //calculate a payment
+    let payment = calcPayment(loanAmount, loanRate, loanTerm);
+
+    //return a list of payment objects 
+    let payments = getPayments(loanAmount, loanRate, loanTerm, payment);
+
+    displayData(payments, loanAmount, payment);
+
+}
+
+//Build the amorization schedule 
+function getPayments(loanAmount, loanRate, loanTerm, payment) {
+    let paymentsArray = [];
+
+    let balance = loanAmount;
+    let totalInterest = 0;
+    let monthlyPrincipal = 0;
+    let monthlyInterest = 0;
+    let monthlyTotalInterest = 0;
+
+    for (let month = 1; month <= loanTerm; month++) {
+
+        monthlyInterest = calcInterest(balance, loanRate);
+        totalInterest += monthlyInterest;
+        monthlyPrincipal = payment - monthlyInterest;
+        balance = Math.abs(balance - monthlyPrincipal);
+
+        //add the details to an object
+        let curPayment = {
+            month: month,
+            payment: payment,
+            principal: monthlyPrincipal,
+            interest: monthlyTotalInterest,
+            totalInterest: totalInterest,
+            balance: balance
+        };
+        paymentsArray.push(curPayment);
+    }
+
+    return paymentsArray;
+}
+
+//display the data to the user 
+function displayData(payments, loanAmount, payement) {
+
+    let tableBody = document.getElementById("scheduleBody");
+
+    let template = document.getElementById("schedule-Template");
+
+    //clears the table of any previous data 
+    tableBody.innerHTML = "";
+
+    //configure currency formatter
+    let currencyFormatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    });
+
+    for (let i = 0; i < payments.length; i++) {
+
+        let payRow = template.content.cloneNode(true);
+        let payCols = payRow.querySelectorAll("td");
+
+        payCols[0].textContent = payments[i].month;
+        payCols[1].textContent = currencyFormatter.format(payments[i].payment.toFixed(2));
+        payCols[2].textContent = currencyFormatter.format(payments[i].principal.toFixed(2));
+        payCols[3].textContent = currencyFormatter.format(payments[i].interest.toFixed(2));
+        payCols[4].textContent = currencyFormatter.format(payments[i].totalInterest.toFixed(2));
+        payCols[5].textContent = currencyFormatter.format(payments[i].balance.toFixed(2));
+
+        tableBody.appendChild(payRow);
+    }
+    //total Interest is in the last row of the payments array. 
+    let totalInterest = payments[payments.length - 1].totalInterest;
+
+    //calculate the total cost 
+    let totalCost = Number(loanAmount) + totalInterest;
+
+    let totalPrincipal = Number(loanAmount);
+
+    let labelPrincipal = document.getElementById("totalPrincipal");
+    labelPrincipal.innerHTML = Number(totalPrincipal).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    });
+
+    let labelInterest = document.getElementById("totalInterest");
+    labelInterest.innerHTML = Number(totalInterest).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    });
+
+    let labelPayment = Document.getElementById("payment");
+    labelPayment.innerHTML = Number(payment).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    });
+
+    let labelTotalCost = docuemnt.getElementById("totalCost");
+    labelTotalCost.innerHTML = Number(totalCost).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    });
+
 }
